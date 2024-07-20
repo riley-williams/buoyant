cc23d151198fa46d753b2ae3e1e5bef8a0751534
Riley Williams <williams.riley@icloud.com>
2024-07-20 13:52:24 -0400

    flip horizontal gradient fraction

--- src/style/horizontal_gradient.rs
+++ src/style/horizontal_gradient.rs
@@ -19,6 +19,6 @@ impl<C: ColorValue> ColorStyle for HorizontalGradient<C> {
 
     fn shade_pixel(&self, x: u16, _: u16, in_bounds: crate::primitives::Size) -> C {
         let fraction = in_bounds.width.saturating_sub(x) as f32 / in_bounds.width as f32;
-        C::interpolate(self.start, self.end, fraction)
+        C::interpolate(self.start, self.end, 1.0 - fraction)
     }
 }
