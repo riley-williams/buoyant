b6a2a823b0dcb18529f8fa6003f7d0670b972e31
Riley Williams <williams.riley@icloud.com>
2024-12-31 16:40:43 -0500

    Capsule

--- src/view/shape.rs
+++ src/view/shape.rs
@@ -1,8 +1,10 @@
+mod capsule;
 mod circle;
 mod rectangle;
 mod rounded_rectangle;
 pub mod style;
 
+pub use capsule::Capsule;
 pub use circle::Circle;
 pub use rectangle::Rectangle;
 pub use rounded_rectangle::RoundedRectangle;
