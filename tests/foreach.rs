0cac7c9a60cc892818d761c079abe4abe9acb563
Riley Williams <williams.riley@icloud.com>
2025-11-07 12:39:39 -0500

    tests for horizontal case

--- tests/foreach.rs
+++ tests/foreach.rs
@@ -1,5 +1,6 @@
 use buoyant::render::Render;
 
+use buoyant::view::scroll_view::{ScrollBarVisibility, ScrollDirection};
 use buoyant::{font::CharacterBufferFont, render_target::FixedTextBuffer, view::prelude::*};
 mod common;
 use common::make_render_tree;
@@ -13,7 +14,7 @@ struct User {
 }
 
 #[test]
-fn foreach_with_inner_wrapping_hstack() {
+fn vertical_foreach_with_inner_wrapping_hstack() {
     let mut users = heapless::Vec::<User, 10>::new();
 
     users
@@ -57,7 +58,7 @@ fn foreach_with_inner_wrapping_hstack() {
 }
 
 #[test]
-fn foreach_leading_aligned() {
+fn vertical_foreach_leading_aligned() {
     let mut users = heapless::Vec::<User, 10>::new();
 
     users
@@ -99,7 +100,7 @@ fn foreach_leading_aligned() {
 }
 
 #[test]
-fn foreach_trailing_aligned() {
+fn vertical_foreach_trailing_aligned() {
     let mut users = heapless::Vec::<User, 10>::new();
 
     users
@@ -141,7 +142,7 @@ fn foreach_trailing_aligned() {
 }
 
 #[test]
-fn foreach_spacing() {
+fn vertical_foreach_spacing() {
     let mut rows = heapless::Vec::<String, 10>::new();
     rows.push("Row 1".to_string()).unwrap();
     rows.push("Row 2".to_string()).unwrap();
@@ -161,7 +162,7 @@ fn foreach_spacing() {
 }
 
 #[test]
-fn foreach_undersized() {
+fn vertical_foreach_undersized() {
     let items = vec!["A", "B", "C", "D"];
 
     let view =
@@ -174,4 +175,83 @@ fn foreach_undersized() {
     assert_eq!(buffer.text[2].iter().collect::<String>(), "          ");
     assert_eq!(buffer.text[3].iter().collect::<String>(), "          ");
     assert_eq!(buffer.text[4].iter().collect::<String>(), "          ");
+}
+
+#[test]
+fn horizontal_foreach_equal_size_fits() {
+    let items = vec!["A", "B", "C", "D", "E", "F"];
+
+    let view = ForEach::<8>::new_horizontal(&items, |name| Text::new(name, &FONT))
+        .with_spacing(1)
+        .foreground_color(' ');
+    let mut buffer = FixedTextBuffer::<11, 2>::default();
+    let tree = make_render_tree(&view, buffer.size(), &mut ());
+    tree.render(&mut buffer, &' ');
+    assert_eq!(buffer.text[0].iter().collect::<String>(), "A B C D E F");
+    assert_eq!(buffer.text[1].iter().collect::<String>(), "           ");
+}
+
+#[test]
+fn horizontal_foreach_variable_size_fits_perfectly() {
+    let items = vec!["Apple", "Bob", "C"];
+
+    let view = ForEach::<8>::new_horizontal(&items, |name| Text::new(name, &FONT))
+        .with_spacing(1)
+        .foreground_color(' ');
+    let mut buffer = FixedTextBuffer::<11, 2>::default();
+    let tree = make_render_tree(&view, buffer.size(), &mut ());
+    tree.render(&mut buffer, &' ');
+    assert_eq!(buffer.text[0].iter().collect::<String>(), "Apple Bob C");
+    assert_eq!(buffer.text[1].iter().collect::<String>(), "           ");
+}
+
+#[test]
+fn horizontal_foreach_variable_size_wraps() {
+    let items = vec!["Apples", "Bo", "Candy"];
+
+    let view = ForEach::<8>::new_horizontal(&items, |name| {
+        Text::new(name, &FONT).multiline_text_alignment(HorizontalTextAlignment::Leading)
+    })
+    .with_spacing(1)
+    .foreground_color(' ');
+    let mut buffer = FixedTextBuffer::<11, 2>::default();
+    let tree = make_render_tree(&view, buffer.size(), &mut ());
+    tree.render(&mut buffer, &' ');
+    assert_eq!(buffer.text[0].iter().collect::<String>(), "App Bo Cand");
+    assert_eq!(buffer.text[1].iter().collect::<String>(), "les    y   ");
+}
+
+#[test]
+fn horizontal_foreach_infinite_offer() {
+    let items = vec!["Apple", "Bo", "Candy"];
+
+    let view = ScrollView::new(
+        ForEach::<8>::new_horizontal(&items, |name| {
+            Text::new(name, &FONT).multiline_text_alignment(HorizontalTextAlignment::Leading)
+        })
+        .with_spacing(1)
+        .foreground_color(' '),
+    )
+    .with_direction(ScrollDirection::Horizontal)
+    .with_bar_visibility(ScrollBarVisibility::Never);
+
+    let mut buffer = FixedTextBuffer::<11, 2>::default();
+    let tree = make_render_tree(&view, buffer.size(), &mut ());
+    tree.render(&mut buffer, &' ');
+    assert_eq!(buffer.text[0].iter().collect::<String>(), "Apple Bo Ca");
+    assert_eq!(buffer.text[1].iter().collect::<String>(), "           ");
+}
+
+#[test]
+fn horizontal_foreach_undersized() {
+    let items = vec!["A", "B", "C", "D"];
+
+    let view = ForEach::<2>::new_horizontal(&items, |name| Text::new(name, &FONT))
+        .with_spacing(1)
+        .foreground_color(' ');
+    let mut buffer = FixedTextBuffer::<10, 2>::default();
+    let tree = make_render_tree(&view, buffer.size(), &mut ());
+    tree.render(&mut buffer, &' ');
+    assert_eq!(buffer.text[0].iter().collect::<String>(), "A B       ");
+    assert_eq!(buffer.text[1].iter().collect::<String>(), "          ");
 }
